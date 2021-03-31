3bd122b3c4601ee5735b3e21b58f30270aad63ff
Mohsen Mehrani <m.mehrani14@gmail.com>
2021-03-31 12:30:44 +0430

    personlized farewell added

--- example.py
+++ example.py
@@ -9,12 +9,16 @@ Created on Wed Mar 31 10:07:25 2021
 import numpy as np
 
 def new_func():
+    
     return
 
-def say_hello(name,last_name):
-    print('Hello!'+ 'Mr./Ms.'+ last_name)
-    print('We hope U doing well ' + name)
+def say_hello(name,last_name,language):
+    if language == 'English':
+        print('Hello!'+ 'Mr./Ms.'+ last_name)
+    elif language == 'Italian':
+        print('Buogiorno!'+ 'Signore/ Signora'+ last_name)
     return
 
 
-say_hello('Marzieh','Bathaee')
+say_hello('Marzieh','Bathaee','Italian')
+say_hello('Abbas', 'Shojakani','English')
